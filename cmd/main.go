a0e21329c900dc3b462825f8c40aebd0bbc75a00
Mike Raver <mike.raver@endurance.com>
2019-07-26 14:04:01 -0600

    Added server parameters for a more robust server.

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,21 @@ import (
     "github.com/mikeraver/go-testing-http/api"
     "log"
     "net/http"
+    "time"
 )
 
 func main() {
 	log.Println("registering http handlers")
     customerApi := api.NewCustomerApi()
     customerApi.RegisterHandlers()
-    //http.HandleFunc("/customer", customerApi.CreateCustomerHandler)
 
     log.Println("starting server on port 8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+
+    s := &http.Server {
+        ReadHeaderTimeout:20 * time.Second,
+        ReadTimeout: 1 * time.Minute,
+        WriteTimeout: 2 * time.Minute,
+        Addr:":8080",
+    }
+    log.Fatal(s.ListenAndServe())
 }
